0933aaeb8cbf2d52feaf30f05fd66e0d74cc7b6c
Takahashiyuuri <haru.isk@gmail.com>
2019-03-06 21:12:53 +0900

    0306

---
+++ human/src/jp/co/internous/action/HumanAddress.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package jp.co.internous.action;
+
+/**
+ * @author testuser
+ *
+ */
+public class HumanAddress {
+
+	public String address;
+	public String getAddress(){
+		address="東京";
+		return address;
+	}
+
+}
---
+++ human/src/jp/co/internous/action/HumanAge.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package jp.co.internous.action;
+
+/**
+ * @author testuser
+ *
+ */
+public class HumanAge {
+
+	public int age;
+	public int getAge(){
+		age = 25;
+		return age;
+	}
+
+}
